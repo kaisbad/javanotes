a6c66f5f70ea234d216bab426e57331126c1efe2
kaisbad <158204041+kaisbad@users.noreply.github.com>
2024-10-07 12:12:52 -0400

    a

--- app.js
+++ app.js
@@ -3,19 +3,20 @@ const students = ["Matt", "Mark", "Josephine", "Thomas"];
 //students.forEach((student) => console.log(student));
 //loop through every element in list
 
-const movie = {
-  title: "Star Wars",
-  release: 1977,
-  actors: ["Mark", "Harrison", ""],
+const movie = [
+  {
+    title: "Star Wars",
+    release: 1977,
+    actors: ["Mark", "Harrison", "Carrie"],
 
-  title,
-};
+    title,
+  },
+  { title: "ahfkuhas", release: 1991, actors: ["Jonathan", "Thomas"] },
+];
 movies.forEach((movie) => console.log(movie.title));
 //filter, to get a new array of movies that match criteria
 //array name DOT Array command then arrow function that accepts singular element of array
 const newMovies = movies.filter((movie) => movie.release > 1990);
 console.log(newMovies);
 
-movies
-    .filter((movie) => movie.release >1990)
-    .
+movies.filter((movie) => movie.release > 1990);
